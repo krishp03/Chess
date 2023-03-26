9fa34e14444c3142e8ed4d2e3c95b14034996360
ROSHAN VARADHAN <rdv34@scarletmail.rutgers.edu>
2023-03-26 22:32:50 +0000

    Knight.java edited online with Bitbucket

--- src/chess/Knight.java
+++ src/chess/Knight.java
@@ -7,10 +7,10 @@ public class Knight extends Piece{
     }
 
     @Override
-    public boolean isLegalMove(Piece[][] board, int x0, int x1, int y0, int y1) {
+    public boolean isLegalMove(int x0, int x1, int y0, int y1) {
         // TODO Auto-generated method stub
-        if (board[x1][y1]!=null){
-            if (this.white==board[x1][y1].white) return false;
+        if (Chess.board[x1][y1]!=null){
+            if (this.white==Chess.board[x1][y1].white) return false;
         }
         return Math.abs(x1-x0) * Math.abs(y1-y0) == 2;
     }
