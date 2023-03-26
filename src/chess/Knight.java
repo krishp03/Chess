cee3364a898b8cc36d11770f4bc617870d35c404
ROSHAN VARADHAN <rdv34@scarletmail.rutgers.edu>
2023-03-26 22:12:29 +0000

    Knight.java edited online with Bitbucket

--- src/chess/Knight.java
+++ src/chess/Knight.java
@@ -7,9 +7,12 @@ public class Knight extends Piece{
     }
 
     @Override
-    public boolean isLegalMove(int x0, int x1, int y0, int y1) {
+    public boolean isLegalMove(Piece[][] board, int x0, int x1, int y0, int y1) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isLegalMove'");
+        if (board[x1][y1]!=null){
+            if (this.white==board[x1][y1].white) return false;
+        }
+        return Math.abs(x1-x0) * Math.abs(y1-y0) == 2;
     }
 
     public String toString(){
