ffab3c43324ae1e56c1b7d50b1da3ffe77303d47
ROSHAN VARADHAN <rdv34@scarletmail.rutgers.edu>
2023-03-26 22:10:35 +0000

    Bishop.java edited online with Bitbucket

--- src/chess/Bishop.java
+++ src/chess/Bishop.java
@@ -7,9 +7,68 @@ public class Bishop extends Piece{
     }
 
     @Override
-    public boolean isLegalMove(int x0, int x1, int y0, int y1) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isLegalMove'");
+    public boolean isLegalMove(Piece[][] board, int x0, int x1, int y0, int y1) {
+        boolean valid = Math.abs(x1-x0) == Math.abs(y1-y0) && x0!=x1;
+        int i=x0;
+        int j=y0;
+        if (valid){
+            if (x1>x0){
+                i++;
+                if (y1>y0){
+                    j++;
+                    while (i<x1 && j<y1){
+                        if (board[i][j]!=null) {
+                            valid=false;
+                            break;
+                        }
+                        i++;
+                        j++;
+                    }
+                }
+                else{
+                    j--;
+                    while (i<x1 && j>y1){
+                        if (board[i][j]!=null) {
+                            valid=false;
+                            break;
+                        }
+                        i++;
+                        j--;
+                    }
+                }
+            }
+            else{
+                i--;
+                if (y1>y0){
+                    j++;
+                    while (i>x1 && j<y1){
+                        if (board[i][j]!=null) {
+                            valid=false;
+                            break;
+                        }
+                        i--;
+                        j++;
+                    }
+                }
+                else{
+                    j--;
+                    while (i>x1 && j>y1){
+                        if (board[i][j]!=null) {
+                            valid=false;
+                            break;
+                        }
+                        i--;
+                        j--;
+                    }
+                }
+            }
+        }
+        if (valid){
+            if (board[x1][y1]!=null){
+                if (this.white==board[x1][y1].white) valid=false;
+            }
+        }
+        return valid;
     }
 
     public String toString(){
