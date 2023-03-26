f38b85a0793385b825c20c1f9dd617663a6aa03c
ROSHAN VARADHAN <rdv34@scarletmail.rutgers.edu>
2023-03-26 22:11:30 +0000

    Rook.java edited online with Bitbucket

--- src/chess/Rook.java
+++ src/chess/Rook.java
@@ -7,9 +7,49 @@ public class Rook extends Piece{
     }
 
     @Override
-    public boolean isLegalMove(int x0, int x1, int y0, int y1) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isLegalMove'");
+    public boolean isLegalMove(Piece[][] board, int x0, int x1, int y0, int y1) {
+        boolean valid = ((y1==y0 && x1!=x0) || (x1==x0 && y1!=y0));
+        if (valid){
+            if (y1<y0){
+                for (int i=y0-1; i>y1; i--){
+                    if (board[x1][i]!=null){
+                        valid=false;
+                        break;
+                    }
+                }
+            }
+            else if(y1>y0){
+                for (int i=y0+1; i<y1; i++){
+                    if (board[x1][i]!=null){
+                        valid=false;
+                        break;
+                    }
+                }
+            }
+            else if (x1<x0){
+                for (int i=x0-1; i>x1; i--){
+                    if (board[i][y1]!=null){
+                        valid=false;
+                        break;
+                    }
+                }
+            }
+            else{
+                for (int i=x0+1; i<x1; i++){
+                    if (board[i][y1]!=null){
+                        valid=false;
+                        break;
+                    }
+                }
+            }
+        }
+        if (valid){
+            if (board[x1][y1]!=null){
+                if (this.white == board[x1][y1].white) valid = false;
+            }
+        }
+        if (valid && !this.hasMoved) this.hasMoved=true;
+        return valid;
     }
 
     public String toString(){
