63ce815eb47a53c0f3f679d672305cd67135db64
ROSHAN VARADHAN <rdv34@scarletmail.rutgers.edu>
2023-03-26 22:42:00 +0000

    King.java edited online with Bitbucket

--- src/chess/King.java
+++ src/chess/King.java
@@ -1,78 +1,20 @@
 package chess;
 
-public class Bishop extends Piece{
+public class King extends Piece{
 
-    public Bishop(boolean white){
+    public King(boolean white){
         super(white);
     }
 
     @Override
     public boolean isLegalMove(int x0, int x1, int y0, int y1) {
-        boolean valid = Math.abs(x1-x0) == Math.abs(y1-y0) && x0!=x1;
-        int i=x0;
-        int j=y0;
-        if (valid){
-            if (x1>x0){
-                i++;
-                if (y1>y0){
-                    j++;
-                    while (i<x1 && j<y1){
-                        if (Chess.board[i][j]!=null) {
-                            valid=false;
-                            break;
-                        }
-                        i++;
-                        j++;
-                    }
-                }
-                else{
-                    j--;
-                    while (i<x1 && j>y1){
-                        if (Chess.board[i][j]!=null) {
-                            valid=false;
-                            break;
-                        }
-                        i++;
-                        j--;
-                    }
-                }
-            }
-            else{
-                i--;
-                if (y1>y0){
-                    j++;
-                    while (i>x1 && j<y1){
-                        if (Chess.board[i][j]!=null) {
-                            valid=false;
-                            break;
-                        }
-                        i--;
-                        j++;
-                    }
-                }
-                else{
-                    j--;
-                    while (i>x1 && j>y1){
-                        if (Chess.board[i][j]!=null) {
-                            valid=false;
-                            break;
-                        }
-                        i--;
-                        j--;
-                    }
-                }
-            }
-        }
-        if (valid){
-            if (Chess.board[x1][y1]!=null){
-                if (this.isWhite()==Chess.board[x1][y1].isWhite()) valid=false;
-            }
-        }
-        return valid;
+        
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isLegalMove'");
     }
 
     public String toString(){
-        return (this.isWhite()) ? "wB":"bB";
+        return (this.isWhite()) ? "wK":"bK";
     }
 
 }
