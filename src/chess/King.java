90c4cf433cfc398d83ac980cc7a75e9d8380572a
ROSHAN VARADHAN <rdv34@scarletmail.rutgers.edu>
2023-03-27 01:53:46 +0000

    fixed everything

--- src/chess/King.java
+++ src/chess/King.java
@@ -10,36 +10,37 @@ public class King extends Piece{
     @Override
     public boolean isLegalMove(int x0, int x1, int y0, int y1) {
         // TODO Auto-generated method stub
-        if (Chess.board[y1][x1]!=null){
-            if (isWhite() == Chess.board[y1][x1].isWhite()) return false;
+        if (Chess.board[x1][y1]!=null){
+            if (isWhite() == Chess.board[x1][y1].isWhite()) return false;
         }
         boolean valid=false;
         if (this.hasMoved()) canCastle=false;
         if (canCastle){
-            if (y1==y0){
-                if (x1==x0+2){
-                    if (!(Chess.board[y0][7] instanceof Rook)) canCastle=false;
-                    else if(Chess.board[y0][7].hasMoved()) canCastle=false;
-                    else if (Chess.board[y0][5]==null && Chess.board[y0][6]==null) valid=true;
+            if (x1==x0){
+                if (y1==y0+2){
+                    if (!(Chess.board[x0][7] instanceof Rook)) canCastle=false;
+                    else if(Chess.board[x0][7].hasMoved()) canCastle=false;
+                    else if (Chess.board[x0][5]==null && Chess.board[x0][6]==null) valid=true;
                 }
-                else if (x1==x0-2){
-                    if (!(Chess.board[y0][0] instanceof Rook)) canCastle=false;
-                    else if(Chess.board[y0][0].hasMoved()) canCastle=false;
-                    else if (Chess.board[y0][1]==null && Chess.board[y0][2]==null && Chess.board[y0][3]==null) valid=false;
+                else if (y1==y0-2){
+                    if (!(Chess.board[x0][0] instanceof Rook)) canCastle=false;
+                    else if(Chess.board[x0][0].hasMoved()) canCastle=false;
+                    else if (Chess.board[x0][1]==null && Chess.board[x0][2]==null && Chess.board[x0][3]==null) valid=true;
                 }
             }
         }
         if (valid) {
-            Chess.board[y1][x1]=Chess.board[y0][x0];
-            Chess.board[y0][x0]=null;
-            if (x1==x0+2) {
-                Chess.board[y0][5] = Chess.board[y0][7];
-                Chess.board[y0][7]=null;
+            Chess.board[x1][y1]=Chess.board[x0][y0];
+            Chess.board[x0][y0]=null;
+            if (y1==y0+2) {
+                Chess.board[x0][5] = Chess.board[x0][7];
+                Chess.board[x0][7]=null;
+                System.out.println("Castled Short");
             }
             else{
-                Chess.board[y0][3] = Chess.board[y0][0];
-                Chess.board[y0][0]=null;
-
+                Chess.board[x0][3] = Chess.board[x0][0];
+                Chess.board[x0][0]=null;
+                System.out.println("Castled Long");
             }
             this.setMoved();
             return true;
@@ -48,8 +49,8 @@ public class King extends Piece{
         if (Math.abs(x1-x0)<=1 && Math.abs(y1-y0)<=1) valid=true;
         if (valid){
             if (!this.hasMoved()) this.setMoved();
-            Chess.board[y1][x1]=Chess.board[y0][x0];
-            Chess.board[y0][x0]=null;
+            Chess.board[x1][y1]=Chess.board[x0][y0];
+            Chess.board[x0][y0]=null;
         }
         return valid;
     }
