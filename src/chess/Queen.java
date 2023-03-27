ad111b41814a04f47dd999f01b8e81e97810fecf
ROSHAN VARADHAN <rdv34@scarletmail.rutgers.edu>
2023-03-27 00:11:28 +0000

    Queen.java edited online with Bitbucket

--- src/chess/Queen.java
+++ src/chess/Queen.java
@@ -8,15 +8,15 @@ public class Queen extends Piece{
 
     @Override
     public boolean isLegalMove(int x0, int x1, int y0, int y1) {
-        if (Chess.board[x1][y1]!=null){
-            if (isWhite() == Chess.board[x1][y1].isWhite()) return false;
+        if (Chess.board[y1][x1]!=null){
+            if (isWhite() == Chess.board[y1][x1].isWhite()) return false;
         }
         boolean validNonDiag = ((y1==y0 && x1!=x0) || (x1==x0 && y1!=y0));
         boolean validDiag = Math.abs(x1-x0) == Math.abs(y1-y0) && x0!=x1;
         if (validNonDiag){
             if (y1<y0){
                 for (int i=y0-1; i>y1; i--){
-                    if (Chess.board[x1][i]!=null){
+                    if (Chess.board[i][x1]!=null){
                         validNonDiag=false;
                         break;
                     }
@@ -24,7 +24,7 @@ public class Queen extends Piece{
             }
             else if(y1>y0){
                 for (int i=y0+1; i<y1; i++){
-                    if (Chess.board[x1][i]!=null){
+                    if (Chess.board[i][x1]!=null){
                         validNonDiag=false;
                         break;
                     }
@@ -32,7 +32,7 @@ public class Queen extends Piece{
             }
             else if (x1<x0){
                 for (int i=x0-1; i>x1; i--){
-                    if (Chess.board[i][y1]!=null){
+                    if (Chess.board[y1][i]!=null){
                         validNonDiag=false;
                         break;
                     }
@@ -40,7 +40,7 @@ public class Queen extends Piece{
             }
             else{
                 for (int i=x0+1; i<x1; i++){
-                    if (Chess.board[i][y1]!=null){
+                    if (Chess.board[y1][i]!=null){
                         validNonDiag=false;
                         break;
                     }
@@ -55,7 +55,7 @@ public class Queen extends Piece{
                 if (y1>y0){
                     j++;
                     while (i<x1 && j<y1){
-                        if (Chess.board[i][j]!=null) {
+                        if (Chess.board[j][i]!=null) {
                             validDiag=false;
                             break;
                         }
@@ -66,7 +66,7 @@ public class Queen extends Piece{
                 else{
                     j--;
                     while (i<x1 && j>y1){
-                        if (Chess.board[i][j]!=null) {
+                        if (Chess.board[j][i]!=null) {
                             validDiag=false;
                             break;
                         }
@@ -80,7 +80,7 @@ public class Queen extends Piece{
                 if (y1>y0){
                     j++;
                     while (i>x1 && j<y1){
-                        if (Chess.board[i][j]!=null) {
+                        if (Chess.board[j][i]!=null) {
                             validDiag=false;
                             break;
                         }
@@ -91,7 +91,7 @@ public class Queen extends Piece{
                 else{
                     j--;
                     while (i>x1 && j>y1){
-                        if (Chess.board[i][j]!=null) {
+                        if (Chess.board[j][i]!=null) {
                             validDiag=false;
                             break;
                         }
@@ -100,6 +100,10 @@ public class Queen extends Piece{
                     }
                 }
             }
+        }
+        if (validNonDiag || validDiag) {
+            Chess.board[y1][x1]=Chess.board[y0][x0];
+            Chess.board[y0][x0]=null;
         }
         return validNonDiag || validDiag;
     }
